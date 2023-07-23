b37c1110669e730ba02812f4ba02311d0113d634
leejeongmin <tmxk0810@gmail.com>
2023-07-24 08:35:44 +0900

    - 집에서 작업 하던 거 push 가 안돼서 일단 했던 코드 복붙 한 거 commit
    2023. 07. 24(월)

--- app/src/main/java/com/todo/todolist/screen/Login.kt
+++ app/src/main/java/com/todo/todolist/screen/Login.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -14,6 +15,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -23,7 +26,10 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.airbnb.lottie.compose.*
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.todo.todolist.*
 import com.todo.todolist.R
 import com.todo.todolist.ui.theme.TodoListTheme
@@ -92,7 +98,7 @@ fun LoginScreen(navController: NavHostController) {
     var password by remember { mutableStateOf("") }
     Spacer(modifier = Modifier.height(40.dp))
     Column(Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally) {
+            horizontalAlignment = Alignment.CenterHorizontally) {
         Box(modifier = Modifier.size(200.dp)) {
             LottieAnimation(
                 composition = composition,
@@ -106,7 +112,7 @@ fun LoginScreen(navController: NavHostController) {
             placeholder = {
                 Text(text = stringResource(id = R.string.email),
                     style = MaterialTheme.typography.bodyMedium)
-            },
+                },
             colors = TextFieldDefaults.textFieldColors(
                 containerColor = Color.LightGray,
                 focusedIndicatorColor = Color.Transparent,
@@ -116,8 +122,11 @@ fun LoginScreen(navController: NavHostController) {
             shape = RoundedCornerShape(12.dp),
             modifier = Modifier
                 .padding(horizontal = 8.dp)
-                .fillMaxWidth()
-        )
+                .fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Next)
+            )
         Spacer(modifier = Modifier.height(20.dp))
         TextField(
             value = password,
@@ -126,7 +135,7 @@ fun LoginScreen(navController: NavHostController) {
             placeholder = {
                 Text(text = stringResource(id = R.string.password),
                     style = MaterialTheme.typography.bodyMedium)
-            },
+                          },
             visualTransformation = PasswordVisualTransformation('*'),
             colors = TextFieldDefaults.textFieldColors(
                 containerColor = Color.LightGray,
@@ -137,13 +146,24 @@ fun LoginScreen(navController: NavHostController) {
             shape = RoundedCornerShape(12.dp),
             modifier = Modifier
                 .padding(horizontal = 8.dp)
-                .fillMaxWidth()
-        )
+                .fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Done
+            )
+            )
         Spacer(modifier = Modifier.height(20.dp))
-        Button(onClick = { loginUser(context as Activity, navController, email, password) },
+        Button(
+            enabled = email.isNotEmpty() && password.isNotEmpty(),
+            onClick = { loginUser(context as Activity, navController, email, password) },
             modifier = Modifier
                 .padding(horizontal = 8.dp)
-                .fillMaxWidth()) {
+                .fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.primary,
+                disabledContainerColor = Color.LightGray
+            ),
+        ) {
             Text(text = stringResource(id = R.string.login))
         }
         Spacer(modifier = Modifier.height(20.dp))
@@ -164,16 +184,47 @@ fun LoginScreen(navController: NavHostController) {
     }
 }
 
-private fun loginUser(context: Activity, navController: NavHostController, email: String, password: String) {
+private fun loginUser(activity: Activity, navController: NavHostController, email: String, password: String) {
     val auth = FirebaseAuth.getInstance()
     auth.signInWithEmailAndPassword(email.trim(), password.trim())
-        .addOnCompleteListener(context) { task ->
+        .addOnCompleteListener(activity) { task ->
             if (task.isSuccessful) {
-                navController.navigate("home")
-                println("로그인 성공")
-            } else {
-                println(task.exception)
-                println("로그인 실패")
+                val user = auth.currentUser
+                val uid = user?.uid ?: ""
+                getUserData(uid, navController)
+            } else println(task.exception)
+        }
+}
+
+private fun getUserData(uid: String, navController: NavHostController) {
+    val userUid = FirebaseDatabase.getInstance().getReference("users")
+    val userEmailRef = userUid.child(uid).child("info").child("email")
+    val userNameRef = userUid.child(uid).child("info").child("name")
+
+    userEmailRef.addListenerForSingleValueEvent(object : ValueEventListener {
+        override fun onDataChange(snapshot: DataSnapshot) {
+            val email = snapshot.getValue(String::class.java)
+            if (email != null) {
+                UserInfo.userEmail = email
+
+                userNameRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        val name = snapshot.getValue(String::class.java)
+                        if (name != null) {
+                            UserInfo.userName = name
+                            if (UserInfo.userEmail.isNotEmpty() && UserInfo.userName.isNotEmpty()) {
+                                navController.navigate("home")
+                            }
+                        }
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                    }
+                })
             }
         }
+
+        override fun onCancelled(error: DatabaseError) {
+        }
+    })
 }
--- app/src/main/java/com/todo/todolist/screen/drawer/Home.kt
+++ app/src/main/java/com/todo/todolist/screen/drawer/Home.kt
@@ -1,9 +1,9 @@
 package com.todo.todolist.screen.drawer
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.*
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,12 +13,11 @@ import androidx.compose.material3.DrawerState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
@@ -32,13 +31,9 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.todo.todolist.R
+import com.todo.todolist.screen.ConfirmDialog
 
-data class UserInfo(val name: String, val email: String) // 사용자 정보 데이터 모델
-
-@Composable
-fun UserData(){
-
-}
+enum class BounceState { Pressed, Released }
 
 @Composable
 fun HomeScreen() {
@@ -88,18 +83,16 @@ fun HomeScreen() {
     doneTodoRef.addValueEventListener(doneEventListener)
 
 
-    Column {
+    Column(modifier = Modifier
+        .verticalScroll(rememberScrollState())
+        .padding(top = 10.dp)) {
         ListName(stringResource(id = R.string.main_todo_list))
         todoListState.value.forEach { todo ->
-            EachList(todo, true, ImageVector.vectorResource(id = R.drawable.ic_uncheck)) {
-                doneTodo(todo)
-            }
+            EachList(false, todo, true, ImageVector.vectorResource(id = R.drawable.ic_uncheck))
         }
         ListName(stringResource(id = R.string.main_done_list))
         doneTodoListState.value.forEach { done ->
-            EachList(done, false, ImageVector.vectorResource(id = R.drawable.ic_check)) {
-                cancelDone(done)
-            }
+            EachList(true, done, false, ImageVector.vectorResource(id = R.drawable.ic_check))
         }
     }
 }
@@ -118,15 +111,39 @@ fun ListName(name: String) {
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun EachList(eachName:String, type: Boolean, image: ImageVector, onClick: () -> Unit) {
-    Column(modifier = Modifier.padding(all = 10.dp)) {
+fun EachList(isDone: Boolean, eachName:String, type: Boolean, image: ImageVector) {
+    var showConfirmDialog by remember { mutableStateOf(false) }
+    val currentState: BounceState by remember { mutableStateOf(BounceState.Released) }
+    val transition = updateTransition(targetState = currentState, label = "animation")
+    val scale: Float by transition.animateFloat(
+        transitionSpec = { spring(stiffness = 900f) }, label = ""
+    ) { state ->
+        if (state == BounceState.Pressed) {
+            0.95f
+        } else {
+            1f
+        }
+    }
+    Column(modifier = Modifier
+        .graphicsLayer {
+            scaleX = scale
+            scaleY = scale
+        }
+        .combinedClickable(
+            onClick = {
+                if(!isDone) doneTodo(eachName)
+                else cancelDone(eachName)
+            },
+            onLongClick = { showConfirmDialog = true }
+        )
+        .padding(all = 10.dp)) {
         Row(verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
                 .border(1.dp, Color.LightGray)
                 .fillMaxWidth()
-                .padding(vertical = 8.dp)
-                .clickable(interactionSource = MutableInteractionSource(), indication = null) { onClick() }) {
+                .padding(all = 8.dp)) {
             Image(imageVector = image,
                 contentDescription = stringResource(id = R.string.check_state))
             Text(text = eachName,
@@ -134,14 +151,24 @@ fun EachList(eachName:String, type: Boolean, image: ImageVector, onClick: () -> 
                 modifier = Modifier.padding(start = 8.dp))
         }
     }
+    if(showConfirmDialog) {
+        ConfirmDialog(
+            onDismiss = { showConfirmDialog = false },
+            content = stringResource(id = R.string.do_delete),
+            confirmAction = {
+                showConfirmDialog = false
+                deleteTodo(eachName) }) {
+            showConfirmDialog = false
+        }
+    }
 }
+
 
 private fun doneTodo(todo: String) {
     val uid = FirebaseAuth.getInstance().currentUser?.uid
     val usersRef = FirebaseDatabase.getInstance().getReference("todo")
     val todoRef = usersRef.child(uid.toString()).child("todo")
     val completeRef = usersRef.child(uid.toString()).child("complete")
-    // todoRef에서 해당 todo 제거
     todoRef.addListenerForSingleValueEvent(object : ValueEventListener {
         override fun onDataChange(dataSnapshot: DataSnapshot) {
             for (childSnapshot in dataSnapshot.children) {
@@ -158,7 +185,6 @@ private fun doneTodo(todo: String) {
         }
     })
 
-    // completeRef에 해당 todo 추가
     val completeId = completeRef.push().key
     if (completeId != null) {
         val newCompleteRef = completeRef.child(completeId)
@@ -193,4 +219,43 @@ private fun cancelDone(todo: String) {
         val newCompleteRef = todoRef.child(todoId)
         newCompleteRef.setValue(todo)
     }
+}
+
+private fun deleteTodo(todo: String) {
+    val uid = FirebaseAuth.getInstance().currentUser?.uid
+    val usersRef = FirebaseDatabase.getInstance().getReference("todo")
+    val todoRef = usersRef.child(uid.toString()).child("todo")
+    val completeRef = usersRef.child(uid.toString()).child("complete")
+
+    todoRef.addListenerForSingleValueEvent(object : ValueEventListener {
+        override fun onDataChange(dataSnapshot: DataSnapshot) {
+            for (childSnapshot in dataSnapshot.children) {
+                val value = childSnapshot.getValue(String::class.java)
+                if (value == todo) {
+                    childSnapshot.ref.removeValue()
+                    break
+                }
+            }
+        }
+
+        override fun onCancelled(databaseError: DatabaseError) {
+            // 에러 처리 로직
+        }
+    })
+
+    completeRef.addListenerForSingleValueEvent(object : ValueEventListener {
+        override fun onDataChange(dataSnapshot: DataSnapshot) {
+            for (childSnapshot in dataSnapshot.children) {
+                val value = childSnapshot.getValue(String::class.java)
+                if (value == todo) {
+                    childSnapshot.ref.removeValue()
+                    break
+                }
+            }
+        }
+
+        override fun onCancelled(databaseError: DatabaseError) {
+            // 에러 처리 로직
+        }
+    })
 }
